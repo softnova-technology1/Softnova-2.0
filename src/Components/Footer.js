1e3d5ff365a60cca3bc82c8041680cfddc4c7944
Your GitHub Username <your-email@example.com>
2026-02-05 13:08:59 +0530

    footerlink05

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -12,9 +12,7 @@ import logo from "../images/softnovaLogo.png";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-     
       <div className={styles.top}>
-       
         <div className={styles.about}>
           <img src={logo} alt="Softnova Logo" className={styles.logoImg} />
           <p>
@@ -24,10 +22,8 @@ const Footer = () => {
           </p>
         </div>
 
-       
         <div className={styles.verticalDivider}></div>
 
-       
         <div className={styles.contact}>
           <h3>Please write to us</h3>
 
@@ -51,58 +47,101 @@ const Footer = () => {
         </div>
       </div>
 
-     
       <div className={styles.horizontalDivider}></div>
 
-     
       <div className={styles.links}>
         <div>
           <h4>Company</h4>
           <ul>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/terms">Terms & Conditions</Link></li>
-            <li><Link to="/services">Services</Link></li>
-            <li><Link to="/GetStarted">Get Quote</Link></li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/terms">Terms & Conditions</Link>
+            </li>
+            <li>
+              <Link to="/services">Services</Link>
+            </li>
+            <li>
+              <Link to="/GetStarted">Get Quote</Link>
+            </li>
           </ul>
         </div>
 
         <div>
           <h4>Helpful Resources</h4>
           <ul>
-            <li><Link to="/products">Products</Link></li>
-            <li><Link to="/career">Careers</Link></li>
-            <li><Link to="/privacy">Privacy Policy</Link></li>
-            <li><Link to="/contact">Contact Us</Link></li>
+            <li>
+              <Link to="/products">Products</Link>
+            </li>
+            <li>
+              <Link to="/career">Careers</Link>
+            </li>
+            <li>
+              <Link to="/privacy">Privacy Policy</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
           </ul>
         </div>
 
         <div>
           <h4>Services</h4>
           <ul>
-            <li><Link to="/services/CloudIT">Cloud and IT</Link></li>
-            <li><Link to="/services/GraphicDesign">Graphic Design</Link></li>
-            <li><Link to="/services/Software">Software Development</Link></li>
-            <li><Link to="/services/OtherServices">Other Services</Link></li>
+            <li>
+              <Link to="/services/CloudIT">Cloud and IT</Link>
+            </li>
+            <li>
+              <Link to="/services/GraphicDesign">Graphic Design</Link>
+            </li>
+            <li>
+              <Link to="/services/Software">Software Development</Link>
+            </li>
+            <li>
+              <Link to="/services/OtherServices">Other Services</Link>
+            </li>
           </ul>
         </div>
 
         <div>
           <h4>Follow Us</h4>
           <div className={styles.social}>
-            <FaFacebookF />
-            <FaWhatsapp />
-            <FaLinkedinIn />
-            <FaInstagram />
+            <a
+              href="https://www.instagram.com/softnovatech/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaInstagram />
+            </a>
+            <a
+              href="https://www.facebook.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaFacebookF />
+            </a>
+            <a
+              href="https://www.linkedin.com/company/softnovatechnology/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaLinkedinIn />
+            </a>
+            <a
+              href="https://api.whatsapp.com/send/?phone=6385118083"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaWhatsapp />
+            </a>
           </div>
         </div>
       </div>
 
       <div className={styles.horizontalDivider}></div>
 
-      
-      <div className={styles.bottom}>
-        © 2025 SOFTNOVA. All Rights Reserved.
-      </div>
+      <div className={styles.bottom}>© 2025 SOFTNOVA. All Rights Reserved.</div>
     </footer>
   );
 };
